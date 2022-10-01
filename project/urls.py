3285811af7e1cee36a29c5c07dc040a6ec611382
aakriti1435 <write10aakriti07@gmail.com>
2022-10-01 20:00:32 +0530

    Initial Apps Setup

--- project/urls.py
+++ project/urls.py
@@ -1,21 +1,27 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf.urls import include, url
+from django.conf import settings
+from accounts.views import  AdminLoginView
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
+    url(r'^admin/login/', AdminLoginView.as_view()),
     path('admin/', admin.site.urls),
-]
+    path('', include('frontend.urls')),
+    path('accounts/', include('accounts.urls', 'accounts')),
+    url('^accounts/', include('django.contrib.auth.urls')),
+    path('backup/', include('backup.urls') , name="Back_up"),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+] 
+
+if settings.DEBUG is False:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+handler404 = 'frontend.views.handler404'
+handler500 = 'frontend.views.handler500'
+handler403 = 'frontend.views.handler403'
+handler400 = 'frontend.views.handler400'
